cccf3346bbf1cedcfcaee806a9106bb52d4e6ff5
Ntountou1 <panosntountou7@hotmail.com>
2025-11-01 18:25:43 +0200

    Include password in Players json

--- QuizGame.Domain/Entities/Player.cs
+++ QuizGame.Domain/Entities/Player.cs
@@ -10,5 +10,6 @@ namespace QuizGame.Domain.Entities
     {
         public int Id { get; set; }
         public string? Username { get; set; }
+        public string? Password { get; set; }
     }
 }
