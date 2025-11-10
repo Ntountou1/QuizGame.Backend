20af6434f3b6c770c2ba19567bffd69041825782
Ntountou1 <panosntountou7@hotmail.com>
2025-11-10 21:12:39 +0200

    Add this aswell for the interface

---
+++ QuizGame.Application/Interfaces/IPlayerService.cs
@@ -0,0 +1,17 @@
+﻿using QuizGame.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Application.Interfaces
+{
+    public interface IPlayerService
+    {
+        IEnumerable<PlayerResponse> GetPlayers();
+        PlayerResponse CreatePlayer(CreatePlayerRequest request);
+        LoginResponse? Login(LoginRequest request);
+        UpdatePasswordResponse? UpdatePassword(int userId, UpdatePasswordRequest request);
+    }
+}
