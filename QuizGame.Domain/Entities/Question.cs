d4d746bd883674ee79c885ddd848e2863a85ab87
Ntountou1 <panosntountou7@hotmail.com>
2025-11-28 13:08:00 +0200

    Add Question and Answer models

---
+++ QuizGame.Domain/Entities/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Domain.Entities
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Difficulty { get; set; } = string.Empty;
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+        public int CorrectAnswerId { get; set; }
+    }
+}
