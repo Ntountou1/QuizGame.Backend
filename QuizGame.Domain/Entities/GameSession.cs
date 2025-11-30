63d18c5f873de9e5432d8f6df2ba05a43dbfdaad
Ntountou1 <panosntountou7@hotmail.com>
2025-11-30 23:11:30 +0200

    Create GameSession model and GameSessionStatus enum

---
+++ QuizGame.Domain/Entities/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Domain.Entities
+{
+    public class GameSession
+    {
+            public int Id { get; set; }
+            public int PlayerId { get; set; }
+            public Player Player { get; set; } = null!;
+            public DateTime StartTime { get; set; }
+            public DateTime? EndTime { get; set; }
+            public int Score { get; set; } = 0;
+            public string Status { get; set; } = "InProgress"; // InProgress, Completed, Abandoned
+            public int TotalQuestions { get; set; } = 5;
+            public int TimeLimitSeconds { get; set; } = 10;
+
+            public List<int> QuestionIds { get; set; } = new List<int>();
+            public TimeSpan? TotalTimeTaken { get; set; }
+     }
+}
+
---
+++ QuizGame.Domain/Enums/GameSessionStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Domain.Enums
+{
+    public enum GameSessionStatus
+    {
+        InProgress,
+        Completed,
+        Abandoned
+    }
+}
