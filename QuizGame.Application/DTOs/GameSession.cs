2e6fa817d061ea308644591f99602e606706ba1e
Ntountou1 <panosntountou7@hotmail.com>
2025-12-07 11:21:29 +0200

    Add DTOs related to GameSession

---
+++ QuizGame.Application/DTOs/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Application.DTOs
+{
+    public class GameSession
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int Score { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public int TotalQuestions { get; set; }
+        public int TimeLimitSeconds { get; set; }
+        public IEnumerable<int> QuestionIds { get; set; } = Array.Empty<int>();
+    }
+}
---
+++ QuizGame.Application/DTOs/StartGameResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Application.DTOs
+{
+    public class StartGameResponse
+    {
+        public int GameSessionId { get; set; }
+        public int PlayerId { get; set; }
+        public DateTime StartTime { get; set; }
+        public int TotalQuestions { get; set; }
+        public int TimeLimitSeconds { get; set; }
+        public IEnumerable<int> QuestionIds { get; set; } = Array.Empty<int>();
+    }
+}
